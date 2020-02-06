91f8c12366833bbc5f74170a9854d8cb2c2e41d4
baplmrkrrck <31512565+baplmrkrrck@users.noreply.github.com>
2020-02-06 18:35:34 -0500

    In class work
    Updated to count dates in class

--- cdlog.py
+++ cdlog.py
@@ -1,5 +1,7 @@
-import sys,time,os,json,datetime
+import sys,time,os,json,datetime,operator
 
+#SELECT * FROM cddata WHERE Date > 2019-01-01 and Date < 2019-01-20
+dateCount = {}
 for root,dirs, files in os.walk('data'):
     #print(root,dirs,files)
     path = root.split(os.sep)
@@ -9,4 +11,24 @@ for root,dirs, files in os.walk('data'):
         f = open(fp, 'r')
         for line in f:
             data = json.loads(line)
-            #print(len(data))
+            payload = data['payload']
+            payload = json.loads(payload)
+#            print(payload['received'])
+            k = payload['received'].split('T')[0]
+            date = k.split("-")
+            if date[0] == '2019' and date[1] == "01" and (date[2] == '01' or date[2] == '02'):
+                print (k)
+                if k in dateCount.keys():
+                    dateCount[k]+=1
+                else:
+                    dateCount[k] = 1
+
+# 0 == key , 1 == value
+sorted_dc = sorted(dateCount.items(),key=operator.itemgetter(1),reverse=True)
+
+print(sorted_dc)
+
+file = 'dateCount.txt'
+f = open(file, mode='a')
+for row in dateCount:
+    f.write(row + "\n")
